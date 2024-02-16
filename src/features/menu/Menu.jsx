bc278c08143a569e1fe834459329450ff8d661ef
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-02-16 18:38:17 +0200

    Create menu loading for router and map all products  from menu

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,3 +1,17 @@
+import { useLoaderData } from 'react-router-dom';
+import {getMenu} from './../../services/apiRestaurant';
+import MenuItem from './MenuItem';
+
 export default function Menu(){
-    return <h1>Menu</h1>;
+    const menu = useLoaderData();
+   return(
+    <ul>
+        {menu.map(pizza=> <MenuItem pizza={pizza} key={pizza.id}/>)}
+    </ul>
+   );
+}
+
+export async function Loader(){
+    const menu = await getMenu();
+    return menu;
 }
