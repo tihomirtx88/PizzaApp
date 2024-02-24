cc3f09d4d6e0f85e3e1f59692fb432570ed7c606
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-02-24 14:21:59 +0200

    Create cart slice

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -8,10 +8,32 @@ const cartSlice = createSlice({
     name: 'cart',
     initialCartstate,
     reducers: {
-      addCart(state, action){},
-      deleteItem(state, action){},
-      increaseItemQuantity(state, action){},
-      decreaseItemQuantity(state, action){},
-      clearCart(state, action){},
+      addCart(state, action){
+        // payload === newItem
+        state.cart.push(action.payload);
+      },
+      deleteItem(state, action){
+        // payload === pizzaId
+        state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+      },
+      increaseItemQuantity(state, action){
+         // payload === pizzaId
+        const item = state.cart.find((item) => item.pizzaId === action.payload);
+        item.quantity++;
+        item.totalPrice = item.quantity * item.unitPrice;
+      },
+      decreaseItemQuantity(state, action){
+           // payload === pizzaId
+           const item = state.cart.find((item) => item.pizzaId === action.payload);
+           item.quantity--;
+           item.totalPrice = item.quantity * item.unitPrice;
+      },
+      clearCart(state){
+        state.cart = [];
+      },
     }
 });
+
+export const {addCart, deleteItem, increaseItemQuantity, decreaseItemQuantity, clearCart} = cartSlice.actions;
+
+export default cartSlice.reducer;
